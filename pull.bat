e6268c512f620f2e04e181071600b542fc16a1c3
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2024-04-15 14:45:34 +0530

    added pull script

---
+++ pull.bat
@@ -0,0 +1,7 @@
+@echo off
+git init
+git remote add origin https://github.com/sanj-04/Hospital-Management-System.git
+@REM git config --local user.name ''
+@REM git config --local user.email '@users.noreply.github.com'
+git pull origin master
+pause
