7a5be4da273000bfe494bc46fcf9da3dba8986c4
rshnGhost <31742263+rshnGhost@users.noreply.github.com>
2024-04-27 01:13:28 +0530

    added Patient Registrations dob phone fields

--- src/dataStorage/signals.py
+++ src/dataStorage/signals.py
@@ -1,9 +1,11 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.conf import settings
 from django.dispatch import receiver
+from registration.signals import user_registered
 from dataStorage.models import Patient, Doctor
 # from .main.password import fetch
 from credentials.credentials import credentials
+from datetime import datetime
 
 # from rest_framework.authtoken.models import Token
 # from django_otp.plugins.otp_totp.models import TOTPDevice
@@ -24,6 +26,16 @@ from credentials.credentials import credentials
 # # qrcode==7.3.1
 # # django-otp==1.1.3
 
+@receiver(user_registered)
+def preRegistrations(sender, user, request, **kwargs):
+    date_of_birth = request.POST.get("date_of_birth")
+    date_of_birthObj = datetime.strptime(date_of_birth, "%d-%b-%Y")
+    phone_number = request.POST.get("phone_number")
+    Patient.objects.create(
+        user = user,
+        date_of_birth=date_of_birthObj.date(),
+        phone_number=phone_number,
+    )
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def postRegistrations(sender, instance, created, **kwargs):
@@ -33,4 +45,8 @@ def postRegistrations(sender, instance, created, **kwargs):
             setting_json = credentials.get("default_setting_json"),
         )
     # if created and not instance.is_staff:
-    #     Patient.objects.create(user=instance)
+    #     Patient.objects.create(
+    #         user = instance,
+    #         date_of_birth=request.session.get("date_of_birth").date(),
+    #         phone_number=request.session.get("phone_number"),
+    #     )
